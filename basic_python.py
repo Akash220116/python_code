ae7d4bcec98c44c713083b5697c9e12589208b4f
Akash220116 <akashasaduzzaman216@gmail.com>
2026-01-08 23:09:41 +0600

    two way of print output

--- basic_python.py
+++ basic_python.py
@@ -11,3 +11,9 @@ print(chr(d))
 
 e ="akash mony"
 print(e[0:5:1])
+
+name ="Mony"
+age =23
+
+print ("my name is",name,"and my age is",age) #process 1
+print(f"my name is {name} and my age is {age}") #process 2
